6cca90963bdbf8a16bfe163dacf0ce349d1dfe9e
Parkjju <rudwns3927@gmail.com>
2026-05-14 11:23:20 -0700

    init: project setup

---
+++ dignify/dignify/dignifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dignifyApp.swift
+//  dignify
+//
+//  Created by 박경준 on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct dignifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
